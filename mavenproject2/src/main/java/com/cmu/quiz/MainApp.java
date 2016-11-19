10d95056ae66bbb118b97235593ffd10401e3461
UrvashiKohli <ukk@andrew.cmu.edu>
2016-11-19 23:09:36 +1030

    Initial

--- mavenproject2/src/main/java/com/cmu/quiz/MainApp.java
+++ mavenproject2/src/main/java/com/cmu/quiz/MainApp.java
@@ -1,46 +1,8 @@
-<<<<<<< HEAD
-package com.cmu.quiz;
-
-import javafx.application.Application;
-import static javafx.application.Application.launch;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-
-public class MainApp extends Application {
-
-    Button button;
-    @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/LoginPage.fxml"));
-        Scene scene = new Scene(root);
-        scene.getStylesheets().add("/styles/Styles.css");
-
-        stage.setTitle("JavaFX and Maven");
-        stage.setScene(scene);
-        stage.show();
-        
-
-         
-    }
 
     /**
      * The main() method is ignored in correctly deployed JavaFX application.
      * main() serves only as fallback in case the application can not be
-     * launched through deployment artifacts, e.g., in IDEs with limited FX
-     * support. NetBeans ignores main().
-     *
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        launch(args);
-    }
-
-}
-=======
+     * launched through deployment artifacts, e.g., in IDEs with */
 package com.cmu.quiz;
 
 import javafx.geometry.Rectangle2D;
@@ -86,4 +48,4 @@ public class MainApp extends Application {
     }
 
 }
->>>>>>> origin/master
+
