678d0ea7f2ce87cbe1d1429d7939f4ecb39a9776
b2bey <b2bey@DESKTOP-AIAO7K0>
2016-11-23 22:47:26 +1030

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	mavenproject2/src/main/java/com/cmu/quiz/Instructor.java
    
    #	mavenproject2/src/main/java/com/cmu/quiz/InstructorReportsDatabase.java
    #	mavenproject2/src/main/java/com/cmu/quiz/Login.java
    #	mavenproject2/src/main/java/com/cmu/quiz/Quiz.java
    #	mavenproject2/src/main/java/com/cmu/quiz/QuizQuestion.java
    #	mavenproject2/src/main/java/com/cmu/quiz/ReadQuestionTextFile.java
    #	mavenproject2/src/main/java/com/cmu/quiz/ReportToPDF.java
    #	mavenproject2/src/main/java/com/cmu/quiz/Reports.java
    #	mavenproject2/src/main/java/com/cmu/quiz/SignUp.java
    #	mavenproject2/src/main/java/com/cmu/quiz/Student.java
    #	mavenproject2/src/main/java/com/cmu/quiz/StudentReportsDatabase.java
    #	mavenproject2/src/main/java/com/cmu/quiz/TextToDatabase.java

--- mavenproject2/src/main/java/com/cmu/quiz/LPDB.java
+++ mavenproject2/src/main/java/com/cmu/quiz/LPDB.java
@@ -18,7 +18,7 @@ import java.util.List;
 
 /**
  *
- * @author b2bey
+ * @author b2
  */
 public class LPDB {
 
@@ -29,25 +29,33 @@ public class LPDB {
         conn = DriverManager.getConnection(dbURL);
         createTable(conn, stmt);
         insertToDB(conn, stmt);
-     int userVal= validateUserExists(conn,stmt,username);
-     int check = 0;
-     if(userVal==1){
-        String sql = "SELECT password FROM APP.USERDATA WHERE userId=?";
-        stmt = conn.prepareStatement(sql);
-        stmt.setString(1, username);
-        ResultSet rs = stmt.executeQuery();
-        rs.next();
-        String pass = rs.getString("PASSWORD");
-        System.out.println(pass);
-        
-        if (!pass.equals(password)) {
-            check = 0;
+        int userVal = validateUserExists(conn, stmt, username);
+        int stuInsId = checkStuIns(conn, stmt, username);
+//        System.out.println("StuInsId is "+stuInsId);
+        int check = 0;
+        if (userVal == 1) {
+            String sql = "SELECT password FROM APP.USERDATA WHERE userid=?";
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, username);
+            ResultSet rs = stmt.executeQuery();
+            rs.next();
+            String pass = rs.getString("PASSWORD");
+//            System.out.println("Correct Password is "+ pass);
+
+            if (!pass.equals(password)) {
+                check = 0;
+            } else {
+                check = 1;
+            }
         } else {
-            check = 1;
-        }}else{
-         check=0;
-     }
-     
+            check = 0;
+        }
+        if (check == 1) {
+            if (stuInsId == 1) {
+                check = 2;
+            }
+        }
+        dropTable(conn, stmt);
         return check;
 
     }
@@ -56,7 +64,9 @@ public class LPDB {
         try {
             String sql = "CREATE TABLE USERDATA("
                     + "userid VARCHAR(20),"
-                    + "password VARCHAR(20))";
+                    + "username VARCHAR(20),"
+                    + "password VARCHAR(20),"
+                    + "stuIns VARCHAR(20))";
             stmt = conn.prepareStatement(sql);
             stmt.executeUpdate();
         } catch (SQLException ex) {
@@ -64,22 +74,31 @@ public class LPDB {
         }
     }
 
+    /**
+     *
+     * @param conn
+     * @param stmt
+     */
     public static void insertToDB(Connection conn, PreparedStatement stmt) {
         try {
             BufferedReader br = new BufferedReader(new FileReader("C:\\B2\\User.txt"));
             String line = null;
             while ((line = br.readLine()) != null) {
                 String[] values = line.split(",");
-                LPDB_var.setUserid(values[1]);
+                LPDB_var.setUserid(values[0]);
+                LPDB_var.setUsername(values[1]);
                 LPDB_var.setPassword(values[2]);
+                LPDB_var.setStuIns(values[3]);
 
                 String addRowSql = "INSERT INTO APP.USERDATA("
-                        + "userid,password)"
-                        + "VALUES(?,?)";
+                        + "userid,username,password,stuins)"
+                        + "VALUES(?,?,?,?)";
 
                 stmt = conn.prepareStatement(addRowSql);
                 stmt.setString(1, LPDB_var.getUserid());
-                stmt.setString(2, LPDB_var.getPassword());
+                stmt.setString(2, LPDB_var.getUsername());
+                stmt.setString(3, LPDB_var.getPassword());
+                stmt.setString(4, LPDB_var.getStuIns());
 
                 stmt.executeUpdate();
             }
@@ -107,4 +126,26 @@ public class LPDB {
         }
         return temp;
     }
+
+    private static int checkStuIns(Connection conn, PreparedStatement stmt, String username) throws Throwable {
+        String sql = "SELECT stuins FROM APP.USERDATA WHERE userid=?";
+        stmt = conn.prepareStatement(sql);
+        stmt.setString(1, username);
+        ResultSet rs = stmt.executeQuery();
+        rs.next();
+        String stuIns = rs.getString("STUINS");
+        int stuInsId = 0;
+//        System.out.println("StuIns is "+stuIns);
+        if (stuIns.equals("instructor")) {
+            stuInsId = 1;
+        }
+        return stuInsId;
+    }
+
+    private static void dropTable(Connection conn, PreparedStatement stmt) throws SQLException {
+        String sql = "DROP TABLE USERDATA";
+        stmt = conn.prepareStatement(sql);
+        stmt.executeUpdate();
+    }
+
 }
