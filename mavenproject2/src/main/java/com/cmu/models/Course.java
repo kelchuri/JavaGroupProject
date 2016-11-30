58cb55246688a7b4b058041445eae781c25973e6
ayushjain1993 <jain.ayush@outlook.com>
2016-11-30 21:48:30 +1030

    implemented java doc for course model class

--- mavenproject2/src/main/java/com/cmu/models/Course.java
+++ mavenproject2/src/main/java/com/cmu/models/Course.java
@@ -15,42 +15,88 @@ public class Course {
     String crs_name;
     Integer ins_id;
 
+    /**
+     * this a default constructor
+     * 
+     */
+    public Course() {
+    }
+    
+    /**
+     * this constructor creates an object of course
+     * 
+     * @param crs_id
+     * @param crs_name
+     * @param ins_id
+     */
     public Course(Integer crs_id, String crs_name, int ins_id) {
         this.crs_id = crs_id;
         this.crs_name = crs_name;
         this.ins_id = ins_id;
     }
 
+    /**
+     * this method is an overriding toString method
+     * 
+     * @return 
+     */
     @Override
     public String toString() {
         return "Course{" + "crs_id=" + crs_id + ", crs_name=" + crs_name + ", ins_id=" + ins_id + '}';
     }
 
+    /**
+     * this method gets the course id
+     * 
+     * @return 
+     */
     public Integer getCrs_id() {
         return crs_id;
     }
 
+    /**
+     * this method sets the course id
+     * 
+     * @param crs_id
+     */
     public void setCrs_id(Integer crs_id) {
         this.crs_id = crs_id;
     }
 
+    /**
+     * this method gets the course name
+     * 
+     * @return 
+     */
     public String getCrs_name() {
         return crs_name;
     }
 
+    /**
+     * this method sets the course name
+     * 
+     * @param crs_name
+     */
     public void setCrs_name(String crs_name) {
         this.crs_name = crs_name;
     }
 
+    /**
+     * this method gets the instructor id
+     * 
+     * @return 
+     */
     public int getIns_id() {
         return ins_id;
     }
 
+    /**
+     * this method sets the instructor id
+     * 
+     * @param ins_id
+     */
     public void setIns_id(int ins_id) {
         this.ins_id = ins_id;
-    }
-
-    public Course() {
     }
 
 }
