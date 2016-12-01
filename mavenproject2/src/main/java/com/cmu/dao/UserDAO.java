2a6edab066fa6f0eddc6dffa09f6d2471f0fe2c4
ayushjain1993 <jain.ayush@outlook.com>
2016-12-02 00:56:19 +1030

    java doc for UserDAO

--- mavenproject2/src/main/java/com/cmu/dao/UserDAO.java
+++ mavenproject2/src/main/java/com/cmu/dao/UserDAO.java
@@ -11,19 +11,82 @@ import com.cmu.models.User;
  * @author kavya
  */
 public interface UserDAO extends AutoCloseable {
+            
+    /**
+     * 
+     * this method adds an User record using an INSERT SQL command
+     * 
+     * @param user
+     * @return 
+     * @throws java.lang.Exception
+     */
     public User add(User user) throws Exception;
-    
+            
+    /**
+     * 
+     * this method is adds the instructor to the database
+     * 
+     * @param user
+     * @return 
+     * @throws java.lang.Exception
+     */
     public User addInstructor(User user) throws Exception;
-
+           
+    /**
+     * 
+     * this method Update an User record with a SQL UPDATE command
+     * 
+     * @param user
+     * @throws java.lang.Exception
+     */ 
     public void update(User user) throws Exception;
-
+            
+    /**
+     * 
+     * this method Find an user record using this ID
+     * 
+     * @param id
+     * @return 
+     * @throws java.lang.Exception
+     */  
     public User findById(int id) throws Exception;
-    
+                
+    /**
+     * 
+     * this method checks if the given user id exists and the password entered is correct
+     * 
+     * @param userId
+     * @param password
+     * @return 
+     * @throws java.lang.Exception
+     */  
     public User checkUserExists(String userId, String password) throws Exception;
-    
+                
+    /**
+     * 
+     * this method checks the user type for the given user id
+     * 
+     * @param userId
+     * @return 
+     * @throws java.lang.Exception
+     */ 
     public String checkUserType(String userId) throws Exception;
-
+                
+    /**
+     * 
+     * this method gets all the students from the user table
+     * 
+     * @return 
+     * @throws java.lang.Exception
+     */ 
     public User[] getAllStudents() throws Exception;
-    
+                 
+    /**
+     * 
+     * this method s all the instructors from user table
+     * 
+     * @return 
+     * @throws java.lang.Exception
+     */  
     public User[] getAllInstructors() throws Exception;
 }
